7e84977916b3c4516b3082c8b9ee3547939dd5ea
Kiran Raj <kiranrajb5882@gamil.com>
2023-11-07 20:58:37 +0530

    final keyworg

---
+++ src/oops/Final.java
@@ -0,0 +1,28 @@
+package oops;
+ class a{
+     final int nums=0;
+      String name;
+      a(){
+
+      }
+       public a(String name){
+           this.name=name;
+       }
+ }
+public class Final {
+    public static void main(String[] args) {
+         final int a=2;
+//         always initialize while declaring variable
+//         a=2; we cant change any value after giving final value
+//       final
+       a b=new a("kiran");
+        b=new a("ooooooo");
+        System.out.println(b.name);
+//        we cant assining another number to b.nums this is why we created final keyword
+//        b.nums=2;
+//       b=new a();
+
+
+
+    }
+}
