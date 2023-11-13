c59f3a003f985daded41a5c931810253ced322b0
Kiran Raj <kiranrajb5882@gmail.com>
2023-11-13 19:15:17 +0530

    added file

--- src/oops/Inner_class.java
+++ src/oops/Inner_class.java
@@ -1,5 +1,7 @@
 package oops;
 
+import java.util.Arrays;
+
 public class Inner_class {
 //    if you dont use static it will be depended on outer class
     static class a{
@@ -11,6 +13,8 @@ public class Inner_class {
 
     public static void main(String[] args) {
         a x=new a("kiran");
+        Arrays.toString(new int[]{1,2,3,4
+        });
         System.out.println(x.name);
     }
 }
