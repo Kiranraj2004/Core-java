d9e04f94df37e91aa4dc85e3ac4ed06091f73756
Kiran Raj <kiranrajb5882@gmail.com>
2023-11-29 22:25:48 +0530

    added

--- src/oops/Inheritance/Boxweigth.java
+++ src/oops/Inheritance/Boxweigth.java
@@ -1,7 +1,7 @@
 package oops.Inheritance;
 
 public class Boxweigth  extends Box{
-    double weigth;
+    double weigth,h;
     Boxweigth(){
         this.weigth=-1;
     }
@@ -16,5 +16,9 @@ public class Boxweigth  extends Box{
         this.weigth=weight;
 //        we cant access the private of parent class to child class
 //        this.l;
+//        System.out.println(super.h);
+//        if you have same object in both class but you have to access the parent class
+//        object then you have to use super keyword
+//        if you use this.h it will initialize in this class it self
     }
 }
--- src/oops/Inheritance/Main.java
+++ src/oops/Inheritance/Main.java
@@ -7,13 +7,22 @@ public class Main {
 //        object we cant access the element
 //        System.out.println(n.l);
         Box d=new Box(2);
-        System.out.println(d.h);
+//        System.out.println(d.h);
         Box a=new Box(d);
 //        System.out.println("copying the values of d box values into a box "+a.l);
-        Boxweigth x=new Boxweigth();
-        Boxweigth q=new Boxweigth(1,2,3,4);
-        System.out.println(q.h);
+//        Boxweigth x=new Boxweigth();
+//        Boxweigth q=new Boxweigth(1,2,3,4);
+//        System.out.println(d.w);
+//        System.out.println(q.h);
+//        Box z=new Boxweigth(3,4,5,1);
+//        we cant access the weight object when we have refrenceing parent class we cant
+//        access the child object
+//        System.out.println(z.weigth);
 
+//     Boxweigth z=new Box();
+//        we cant reference the child class to parent class
+//        but in the above example we can reference the parent to child but you cant
+//        access the child class object
     }
 
 }
