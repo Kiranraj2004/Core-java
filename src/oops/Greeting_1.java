6b65def6176314e6ee64872130d127cfb2b3c8cd
Kiran Raj <kiranrajb5882@gmail.com>
2023-12-04 16:36:47 +0530

    added

--- src/oops/Greeting_1.java
+++ src/oops/Greeting_1.java
@@ -1,5 +1,7 @@
 package oops;
 //import  oops.polymorphism.Shapes.area;
+
+//we can only import the static methods from other packages
 public class Greeting_1 {
     public static void main(String[] args) {
     Greeting_1 s=new Greeting_1();
--- src/oops/Static.java
+++ src/oops/Static.java
@@ -1,9 +1,20 @@
 package oops;
 
 public class Static {
+    static int a;
     public static void main(String[] args) {
-        System.out.println("hello world" );
-        fun();
+//        System.out.println("hello world" );
+//        fun();
+        Static d=new Static();
+        d.a=2;
+        System.out.println(d.a);
+        Static w=new Static();
+        w.a=0;
+        System.out.println(d.a);
+//        static variable is same for every object if you change from one variable
+//        automatically it will change for another variable
+
+
     }
 static void fun(){
 //        this methods are not depended on object
