36cf634ff846d172a99a07ddc828581e3bd35da5
Kiran Raj <kiranrajb5882@gmail.com>
2023-12-05 20:38:06 +0530

    access modifier

---
+++ src/oops/Access/Subclass1.java
@@ -0,0 +1,9 @@
+package oops.Access;
+
+public class Subclass1 extends accessmodifier{
+//    this subclass which going to inherited by parent class
+
+
+
+
+}
---
+++ src/oops/Access/accessmodifier.java
@@ -0,0 +1,24 @@
+package oops.Access;
+
+public class accessmodifier {
+//   this is the main class
+   protected int protec;
+   public int pub;
+   String defult;
+   private int priva;
+//   we can access the protected member in any where except
+//    class created in different package but which is not a subclass
+//   accessmodifier(int data){
+//       this.protec =data;
+//   }
+   void setters(int data){
+      this.priva=data;
+   }
+   int  getters(){
+      return this.priva;
+   }
+
+   public static void main(String[] args) {
+//      there we can access the all the access modified member
+   }
+}
