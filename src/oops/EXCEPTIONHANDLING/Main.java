a58e46fba5c7079db4e8d02b0ab55cf88a85dd91
Kiran Raj <kiranrajb5882@gmail.com>
2023-12-14 20:57:25 +0530

    exceptionalhandling

---
+++ src/oops/EXCEPTIONHANDLING/Main.java
@@ -0,0 +1,27 @@
+package oops.EXCEPTIONHANDLING;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=0;
+        int b=10;
+//        this is the uncheck exceptional handling here  the number is not going to
+//        divide from 0
+        try{
+
+          divide(a,b);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("this fill always executed");
+        }
+
+    }
+    static int divide(int a,int b){
+        if (a==0){
+            throw new RuntimeException();
+        }
+        return b/a;
+    }
+
+}
